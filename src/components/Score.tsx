b677e017a9498ba38c866f52a634c7b08bdf2a1d
Malik Abdul Wahab <malikwahab34@gmail.com>
2024-01-27 23:17:12 +0500

    added chips

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -2,8 +2,10 @@ import { useAppSelector } from "../../hooks/hooks";
 export default function RetriesCount() {
   const score = useAppSelector((state) => state.quiz.score);
   return (
-    <div className="w-fit items-center justify-center whitespace-nowrap rounded-lg bg-chipBlue py-1.5 px-3 font-sans text-lg uppercase text-white">
-      <span className="">Score: {score}</span>
+    <div className="w-fit items-center justify-center whitespace-nowrap rounded-lg bg-chipBlue py-1.5 px-3 font-sans text-lg uppercase text-white cursor-pointer transition duration-300 transform hover:scale-110">
+      <span className="">
+        Score: <span className="text-green-500 font-bold">{score}</span>
+      </span>
     </div>
   );
 }
